00af641a142807583df31ec6c6fb64c86cdb9d79
wlchen <chen.weilun@advancesoft.jp>
2026-01-09 15:06:46 +0900

    Cleanup: apply .gitignore to remove unwanted files

--- build/CMakeFiles/gpu_interface.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "modules/device_interface.mod"
-  "modules/DEVICE_INTERFACE.mod"
-  "CMakeFiles/gpu_interface.dir/device_interface.mod.stamp"
-  )
